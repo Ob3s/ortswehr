e881cb6e11b6a4b8e293f14782194899c3148076
Ob3s <gevin.pust@googlemail.com>
2026-03-13 11:38:27 +0100

    v2.6.9
    - Status-Lampe: Tippen zeigt Detail-Liste aller PrÃ¼fungen
    - Status-Lampe: FCM-Token wird automatisch erneuert wenn veraltet
    - Doppel-Benachrichtigung behoben: SW zeigt keine Notification wenn App offen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js – Service Worker v5
-const CACHE = 'ortswehr-2.6.8';
+const CACHE = 'ortswehr-2.6.9';
 const CACHE_ONLY_ASSETS = [
   '/ortswehr/icons/icon-192.png',
   '/ortswehr/icons/icon-512.png',
